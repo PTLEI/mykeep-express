a083799fbec29dddec37a60375445727688eebc4
PTLEI <541811902@qq.com>
2019-08-29 12:36:12 +0800

    修改数据库配置

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,9 +1,9 @@
 module.exports =
     {
         mysql: {
-            host: 'localhost',
-            user: 'root',
-            password: '123456',
+            host: '120.79.59.91',
+            user: 'qiao',
+            password: 'password123!PASSWORD',
             database: 'mykeep', // 前面建的user表位于这个数据库中 
             port: 3306
         }
