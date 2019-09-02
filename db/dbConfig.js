7ce3fcf25abb4c0a1109d6e31b7df5d697aa06a9
PTLEI <541811902@qq.com>
2019-09-02 23:48:32 +0800

    modify

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,9 +1,9 @@
 module.exports =
     {
         mysql: {
-            host: '120.79.59.91',
-            user: 'qiao',
-            password: 'password123!PASSWORD',
+            host: '114.55.100.41',
+            user: 'root',
+            password: '123456',
             database: 'mykeep', // 前面建的user表位于这个数据库中 
             port: 3306
         }
