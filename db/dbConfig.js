ec82f510e3024dd8e5aea2e2f14600834b8591ba
PTLEI <541811902@qq.com>
2019-09-24 21:16:55 +0800

    添加备案号

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,10 +1,19 @@
 module.exports =
     {
         mysql: {
-            host: '114.55.100.41',
+            host: 'localhost',
             user: 'root',
             password: '123456',
             database: 'mykeep', // 前面建的user表位于这个数据库中 
             port: 3306
         }
     };
+    // {
+    //     mysql: {
+    //         host: '114.55.100.41',
+    //         user: 'root',
+    //         password: '123456',
+    //         database: 'mykeep', // 前面建的user表位于这个数据库中 
+    //         port: 3306
+    //     }
+    // };
